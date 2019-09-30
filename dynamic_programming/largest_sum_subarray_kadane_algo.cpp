a9f00a403b956a9a79335ff09775f6e7890923f9
nikhilsingh050 <35637047+nikhilsingh050@users.noreply.github.com>
2019-09-30 10:46:18 +0530

    Update largest_sum_subarray_kadane_algo.cpp

--- dynamic_programming/largest_sum_subarray_kadane_algo.cpp
+++ dynamic_programming/largest_sum_subarray_kadane_algo.cpp
@@ -18,9 +18,41 @@ int maxSubArray(vector<int>& nums)
     return maxx;
 }
 
+
+//Find the index of maximum subarray:
+//keep track of current subarray index
+//whenever a maximum length is found -> update max start and end.
+void maxSubArrayIndex(vector<int>& a)
+{
+    int last_max=a[0],maxx=a[0],start=0,end=0,mstart=0,mend=0;
+    for(int i=1;i<a.size();i++)
+    {
+        if(a[i]>a[i]+last_max)
+        {
+            last_max=a[i];
+            start=i;
+            end=i;
+        }
+        else
+        {
+            last_max=a[i]+last_max;
+            end=i;
+        }
+
+        if(maxx<last_max)
+        {
+            maxx=last_max;
+            mstart=start;
+            mend=end;
+        }
+    }
+    cout<<"Maximum subarray index: "<<mstart<<" : "<<mend;
+}
+
 int main()
 {
     vector<int> nums={-2,1,-3,4,-1,2,1,-5,4};
-    cout<<maxSubArray(nums);
+    cout<<"Maximum subaarray length: "<<maxSubArray(nums)<<endl;
+    maxSubArrayIndex(nums);
     return 0;
 }
