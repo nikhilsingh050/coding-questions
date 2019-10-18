f734726bdc31b34d638f25042f8b8da379d109dd
nikhilsingh050 <35637047+nikhilsingh050@users.noreply.github.com>
2019-10-19 04:11:05 +0530

    Update palindrome_linked_list.cpp

--- linked_list/palindrome_linked_list.cpp
+++ linked_list/palindrome_linked_list.cpp
@@ -1,3 +1,48 @@
+//Recursive solution:
+
+//use two pointers.
+//one will go till last node with recursion stack
+//one will go next with each comparison.
+
+//Time Complexity: O(n)
+//Space Complexity: O(1) , O(n) recursion stack space
+
+bool pal(ListNode **left, ListNode *right)
+{
+    if(right==NULL)
+        return true;
+
+    bool res = pal(left, right->next);
+    if(res==false)
+        return false;
+
+    if((*left)->val==right->val)
+    {
+        *left=(*left)->next;
+            return true;
+    }
+    return false;
+
+}
+
+
+bool isPalindrome(ListNode* head)
+{
+    return pal(&head,head);
+}
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////
+
+//O(n), O(1) solution:
+
+//1. divide in two parts
+//2. reverse the second half
+//3. compare the two parts
+
 bool isPalindrome(ListNode* head) {
 
     //divide list in two parts
@@ -29,7 +74,7 @@ bool isPalindrome(ListNode* head) {
     secondHalf=prev;
 
 
-    //compare both the halves
+    //compare element of both the halves
     while(secondHalf!=NULL)
     {
         if(firstHalf->val != secondHalf->val)
