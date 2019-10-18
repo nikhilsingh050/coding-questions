665b234d77b06d86c019690eeca56f27620095e7
nikhilsingh050 <35637047+nikhilsingh050@users.noreply.github.com>
2019-10-19 02:53:42 +0530

    Add files via upload

---
+++ palindrome_linked_list.cpp
@@ -0,0 +1,41 @@
+bool isPalindrome(ListNode* head) {
+
+    //divide list in two parts
+    ListNode *slow=head,*fast=head,*firstHalf=head,*secondHalf;
+
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+
+    if(fast==NULL)
+        secondHalf=slow;
+    else
+        secondHalf=slow->next;
+
+
+    //reverse second half
+    ListNode *current,*prev,*next;
+    current=secondHalf;
+    prev=NULL;
+    while(current!=NULL)
+    {
+        next=current->next;
+        current->next=prev;
+        prev=current;
+        current =next;
+    }
+    secondHalf=prev;
+
+
+    //compare element of both the halves
+    while(secondHalf!=NULL)
+    {
+        if(firstHalf->val != secondHalf->val)
+            return false;
+        firstHalf=firstHalf->next;
+        secondHalf=secondHalf->next;
+    }
+    return true;
+}
