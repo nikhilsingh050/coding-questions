5eb57076566742abd23447c30a19ee9174cb7351
nikhilsingh050 <35637047+nikhilsingh050@users.noreply.github.com>
2019-10-20 02:58:41 +0530

    Add files via upload

---
+++ height_of_binary_tree.cpp
@@ -0,0 +1,30 @@
+int maxDepth(TreeNode* root)
+{
+    if(root==NULL)
+        return -1;
+
+    return 1+max(maxDepth(root->left),maxDepth(root->right));
+}
+
+
+
+
+///////////////////////////////////////////////////////////////
+
+//easy to understand
+
+int maxDepth(TreeNode* root)
+{
+    //height of NULL is -1 so that height of child node will be 0
+    if(root==NULL)
+        return -1;
+
+    //height of left sub-tree
+    int l = maxDepth(root->left);
+
+    //height of right sub tree
+    int r = maxDepth(root->right);
+
+    // 1 + max of left and right tree height
+    return 1+max(l,r);
+}
