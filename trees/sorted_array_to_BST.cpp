cab7f10828c91c06a952ade88684710cc6cdb2ff
nikhilsingh050 <35637047+nikhilsingh050@users.noreply.github.com>
2019-10-22 19:24:15 +0530

    Update sorted_array_to_BST.cpp

--- trees/sorted_array_to_BST.cpp
+++ trees/sorted_array_to_BST.cpp
@@ -1,3 +1,5 @@
+//Convert a given sorted array to BST.
+
 TreeNode* growTree(vector<int> &nums, int i, int j)
 {
     if(i>j)
@@ -15,3 +17,55 @@ TreeNode* growTree(vector<int> &nums, int i, int j)
 TreeNode* sortedArrayToBST(vector<int>& nums) {
     return growTree(nums,0,nums.size()-1);
 }
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////
+
+//Noob Approach: First Attempt
+
+/*
+    void growTree(TreeNode* head, vector<int> &nums, int i, int j)
+    {
+        int mid = (i+j)/2;
+        head->val=nums[mid];
+
+        //for single element
+        if(i==j)
+        {
+            head->left=NULL;
+            head->right=NULL;
+            return;
+        }
+
+        //if left child exists
+        if(i!=mid)
+        {
+            head->left=new TreeNode(0);;
+            growTree(head->left, nums, i, mid-1);
+        }
+        else
+            head->left=NULL;
+
+        //if right child exists
+        if(j!=mid)
+        {
+            head->right=new TreeNode(0);;
+            growTree(head->right, nums, mid+1, j);
+        }
+        else
+            head->right=NULL;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if(nums.size()==0)
+            return NULL;
+        TreeNode *head= new TreeNode(0);
+        growTree(head,nums,0,nums.size()-1);
+        return head;
+    }
+*/
